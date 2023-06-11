51ee4e57314a52531d4509d6fa720b32d45bf115
rezolyshon <100044088+rezolyshon@users.noreply.github.com>
2023-06-11 21:55:07 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 25
+// int a = 3;
+// int b = 5;
+// int result = 1;
+// for (int i = 0; i < b; i++)
+// {
+// result *= a;
+// }
+// Console.WriteLine(result);
+
+// int a = 2;
+// int b = 4;
+//  int result = 1;
+// for (int i = 1; i <= b; i++)
+// {
+// result *= a;
+// }
+// Console.WriteLine(result);
+
+// Задача 27
+
+// class Program {
+//     static void Main(string[] args) {
+//         Console.Write("Введите число: ");
+//         int number = int.Parse(Console.ReadLine());
+//         int sum = 0;
+
+//         while (number > 0) {
+//             sum += number % 10;
+//             number /= 10;
+//         }
+
+//         Console.WriteLine("Сумма цифр равна: " + sum);
+//     }
+// }
+
+
+// Задача 29
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         int[] array1 = { 1, 2, 5, 7, 19 };
+//         int[] array2 = { 6, 1, 33 };
+
+//         Console.WriteLine("Массив 1: [{0}]", string.Join(", ", array1));
+//         Console.WriteLine("Массив 2: [{0}]", string.Join(", ", array2));
+
+//         Console.ReadLine();
+//     }
+// }
